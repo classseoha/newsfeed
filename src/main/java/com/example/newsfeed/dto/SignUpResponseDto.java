d7ce22f8091535f9c01516b77d1b48da60238dc2
남유리 <skadbfl11225@gmail.com>
2025-04-08 20:23:09 +0900

    회원가입 구현 보안/유효성 추과

---
+++ src/main/java/com/example/newsfeed/dto/SignUpResponseDto.java
@@ -0,0 +1,37 @@
+package com.example.newsfeed.dto;
+
+import com.example.newsfeed.entity.User;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+public class SignUpResponseDto {
+
+    private final String email;
+    private final String nickname;
+    private final LocalDate birthDate;
+//    private final String password;
+    private final Character gender;
+    private final String image;
+
+    public SignUpResponseDto(
+            String email, String nickname,
+            LocalDate birthDate, Character gender, String image
+    ) {
+        this.email = email;
+        this.nickname = nickname;
+//        this.password = password;
+        this.birthDate = birthDate;
+        this.gender = gender;
+        this.image = image;
+    }
+
+    public SignUpResponseDto(User user){
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+        this.birthDate = user.getBirthDate();
+        this.gender = user.getGender();
+        this.image = user.getImage();
+    }
+}
