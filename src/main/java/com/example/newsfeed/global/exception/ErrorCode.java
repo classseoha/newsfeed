7c34dc94cbaf799c228a39752ac08234ecf7d7f2
seoha <classseoha@gmail.com>
2025-04-10 21:25:39 +0900

    Feat : 예외처리 구현

--- src/main/java/com/example/newsfeed/global/exception/ErrorCode.java
+++ src/main/java/com/example/newsfeed/global/exception/ErrorCode.java
@@ -13,13 +13,17 @@ public enum ErrorCode {
     ENTITY_NOT_FOUND(400, "Bad Request", "C003", "Entity Not Found"),
     INTERNAL_SERVER_ERROR(500, "Server Error", "C004", "Internal Server Error"),
     INVALID_TYPE_VALUE(400, "Bad Request", "C005", "Invalid Type Value"),
-    
+    EMPTY_CONTENTS(204, "No Content", "C006", "No contents"),
+
     // User
     EMAIL_DUPLICATION(400, "Bad Request", "U001", "Email is Duplicated"),
     USER_NOT_FOUND(404, "Not Found", "U002", "User Not Found"),
+    UNAUTHORIZED_USER(403, "FORBIDDEN", "U003", "Unauthorized User"),
+    TOKEN_NOT_VALID(401, "Unauthorized", "U004", "Unauthorized User"),
 
-    // Schedule
-    SCHEDULE_NOT_FOUND(404, "Not Found", "S001", "Schedule Not Found");
+    // Board
+    BOARD_NOT_FOUND(404, "Not Found", "B001", "Board Not Found"),
+    CONSTRAINT_VIOLATION(409, "	Conflict", "B002", "Mandatory input not entered");
 
     private final int status;
     private final String error;
