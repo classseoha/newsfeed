72f48c4358855684f0d4e143f33903a95151888e
janghwal <jhp337037@gmail.com>
2025-04-10 15:51:38 +0900

    fix: 자기 자신 친구 추가 불가능하게 변경

--- src/main/java/com/example/newsfeed/friends/service/RelationService.java
+++ src/main/java/com/example/newsfeed/friends/service/RelationService.java
@@ -6,7 +6,9 @@ import com.example.newsfeed.friends.dto.RelationResponseDto;
 import com.example.newsfeed.friends.repository.RelationRepository;
 import com.example.newsfeed.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -33,12 +35,18 @@ public class RelationService {
                 .collect(Collectors.toList());
     }
     public void sendFriendRequest(String fromEmail, String toEmail) {
+
+        if(fromEmail.equals(toEmail)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         User fromUser = userRepository.findByIdOrElseThrow(fromEmail);
         User toUser = userRepository.findByIdOrElseThrow(toEmail);
 
         if(relationRepository.existsByFollowingEmailAndFollowedEmail(fromUser, toUser)) {
             throw new IllegalArgumentException("이미 친구 요청을 보냈습니다.");
         }
+
 
         Relation relation = new Relation();
         relation.setFollowingEmail(fromUser);
