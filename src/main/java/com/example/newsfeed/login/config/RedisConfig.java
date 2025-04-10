dbc4d6356a441819ddd402e1d893a913d3a99bb1
iop0217 <iop0617.27@gmail.com>
2025-04-10 22:14:48 +0900

    Feat: 예외처리 추가

--- src/main/java/com/example/newsfeed/login/config/RedisConfig.java
+++ src/main/java/com/example/newsfeed/login/config/RedisConfig.java
@@ -2,6 +2,7 @@ package com.example.newsfeed.login.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
